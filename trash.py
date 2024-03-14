3e69e405e0115ee99f17dbe7e7a604ec36c5f31b
kaylanelira <kgl@cin.ufpe.br>
2024-03-14 14:44:58 -0300

    feat(background): add background

--- trash.py
+++ trash.py
@@ -8,7 +8,11 @@ class LixoEspacial(pygame.sprite.Sprite):
         # Lista de nomes de arquivos de imagem disponíveis para o lixo espacial
         trash_images = ["sprites/Trash/Trash1.png", "sprites/Trash/Trash2.png", "sprites/Trash/Trash3.png"]
 
-        self.image = pygame.image.load(random.choice(trash_images))
+        original_image = pygame.image.load(random.choice(trash_images))
+        original_width, original_height = original_image.get_size()
+        scaled_width = original_width // 2
+        scaled_height = original_height // 2
+        self.image = pygame.transform.scale(original_image, (scaled_width, scaled_height))
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, 5) * 120 # Posição inicial aleatória dentro das faixas permitidas
         self.rect.y = -100  # Começa no topo da tela
